354a7cbaba3ded2eb145e02b6248c58c03712e9f
cstothart <cary.stothart@gmail.com>
2020-03-04 07:10:43 -0600

    Added 404 catch all.

--- app.js
+++ app.js
@@ -8,5 +8,7 @@ const app = express();
 app.use('/submissions', submissionsRoutes);
 app.use('/comments', commentsRoutes);
 
+app.get('*', (req, res) => res.status(404).send('<strong>Not Found</strong>'));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is running on ${port}.`));
