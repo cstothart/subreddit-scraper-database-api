1679f3fa295ded0cdb93ddb27b5b8e7587825b77
cstothart <cary.stothart@gmail.com>
2020-03-04 09:06:46 -0600

    Added CORS.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 
 const submissionsRoutes = require('./components/submissions/submissionsRoutes');
 const commentsRoutes = require('./components/comments/commentsRoutes');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/submissions', submissionsRoutes);
 app.use('/comments', commentsRoutes);
