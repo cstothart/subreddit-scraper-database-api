7ced57dbe156c67c76294711670fbd1519cb0166
cstothart <cary.stothart@gmail.com>
2020-07-13 11:19:15 -0500

    Added IE11 support message.

--- public/main.js
+++ public/main.js
@@ -3,3 +3,9 @@ const lastUpdatedDate = document.querySelector('#last_updated_date');
 const newDate = new Date(lastUpdatedDate.textContent);
 
 lastUpdatedDate.textContent = newDate;
+
+if(!(window.ActiveXObject) && "ActiveXObject" in window) {
+  document.getElementById('ie').style.display = 'block';
+} else {
+  document.getElementById('no-ie').style.display = 'block';
+}
